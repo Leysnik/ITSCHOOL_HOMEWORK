1846a940a635350cf1e4244c7584dffb7f335066
Leysnik <77565343+Leysnik@users.noreply.github.com>
2022-10-31 02:04:40 +0800

    ДЗ дата 25.10.22

---
+++ src/com/Fraction/DotOnEdge.java
@@ -0,0 +1,36 @@
+package com.Fraction;
+
+public class DotOnEdge {
+    protected int x;
+    protected int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public DotOnEdge(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public DotOnEdge(){
+        x = 0;
+        y = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +x+"; " + y +')';
+    }
+}
