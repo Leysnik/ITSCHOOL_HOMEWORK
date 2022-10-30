a4a365e8055bb2eefee5db99eee58e093462d889
Leysnik <77565343+Leysnik@users.noreply.github.com>
2022-10-31 01:55:03 +0800

    ДЗ модуль 2.2.2

--- src/com/Fraction/Main.java
+++ src/com/Fraction/Main.java
@@ -4,22 +4,15 @@ import java.util.Scanner;
 
 public class Main {
      public static void  main(String[] strings) {
-        Fraction fraction1 = new Fraction(4,50);
-        Fraction fraction2 = new Fraction(4,50);
-        fraction2.multyKoeff(5);
-        System.out.println(fraction2);
-        Fraction fraction3;
-        fraction3 = fraction1.multyFraction(fraction2);
+         Employee e = new Employee("11",2,"3");
 
-        System.out.println(fraction3);
-        Fraction fraction4;
-        fraction4 = fraction3.demultyFraction(fraction1);
-         System.out.println(fraction4);
-
-         Fraction fraction5 = fraction1.minusFraction(fraction4);
-         System.out.println(fraction5);
-         Fraction fraction6 = fraction1.plusFraction(fraction4);
-         System.out.println(fraction6);
+         System.out.println(e);
+         Vector2D v = new Vector2D(5,7);
+         v.scale(1.5);
+         v.normalized();
+         v.print();
+         System.out.println(v.length());
+         System.out.println();
      }
 
 }
